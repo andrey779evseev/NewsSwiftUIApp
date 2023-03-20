315fc6bd192f72ff34d71270a7f3202addc761d1
andrew <andrey779evseev@gmail.com>
2023-03-20 21:13:03 +0200

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Andrew on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
